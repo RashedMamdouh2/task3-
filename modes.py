193f294f159599aeca299f8783f790a61996b239
zeyad-wail <zeyadalshakhy@gmail.com>
2024-11-13 15:44:16 +0200

    change spectrogram_plot function in modes

--- modes.py
+++ modes.py
@@ -62,7 +62,7 @@ class modes:
 
         fft_result, frequencies = self.compute_fft()
         modified_fft = fft_result.copy()
-        f_max = self.sampling_rate / 2
+        # f_max = self.sampling_rate / 2
         low, high = self.band_edges[slider_idx][0],self.band_edges[slider_idx][1]
         band_mask = (abs(frequencies) >= low) & (abs(frequencies) < high)
         self.gain[slider_idx]= slider_value
@@ -85,19 +85,30 @@ class modes:
         self.window.frequencyDomainPlot.add_signal(signal=signal,start=False,color='r')#old ui
 
 
-
-
-
                                # y        multiplot
     def plot_spectrogram(self, signal, canvas):
         """
         Plot spectrogram using Matplotlib.
         """
-        print("I'm here")
         f, t, Sxx = spectrogram(signal, fs=self.sampling_rate, nperseg=128, noverlap=64)
+        Sxx_db = 10 * np.log10(Sxx + 1e-10)  # Add small value to avoid log(0)
+
+        if canvas.no_label:
+            canvas.no_label = False
+            canvas.vmin, canvas.vmax = np.min(Sxx_db), np.max(Sxx_db)
+
         canvas.axes.clear()
-        canvas.axes.pcolormesh(t, f, 10 * np.log10(Sxx), shading='gouraud')
+        cax = canvas.axes.pcolormesh(t, f, 10 * np.log10(Sxx_db), shading='gouraud', cmap='plasma', vmin=canvas.vmin, vmax=canvas.vmax)
         canvas.axes.set_xlabel("Time (s)")
         canvas.axes.set_ylabel("Frequency (Hz)")
         canvas.axes.set_title("Spectrogram")
+
+        if not hasattr(canvas, 'colorbar') or canvas.colorbar is None:
+            canvas.colorbar = canvas.figure.colorbar(cax, ax=canvas.axes)
+            canvas.colorbar.set_label("Power (dB)")
+                
+        else:
+                # Update the color bar's content
+            canvas.colorbar.update_normal(cax)
+
         canvas.draw()
